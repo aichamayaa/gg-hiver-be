420a43221b6fd7fb54c8bdbb480e7e013c0d12ef
Renan Cavalcanti <renan_cavalcanti@hotmail.com>
2026-03-25 20:12:54 -0400

    chore: get me & auth middleware

---
+++ src/middlewares/authMiddleware.js
@@ -0,0 +1,30 @@
+const jwt = require("jsonwebtoken");
+
+const authMiddleware = (req, res, next)=>{
+
+    const authHeader = req.headers.authorization
+
+    if(!authHeader || !authHeader.startsWith("Bearer ")){
+        return res.status(401).json({
+            message: "Authorization token is missing."
+        });
+    }
+
+    const token = authHeader.split(" ")[1];
+
+    try{
+        const jwtSecret = "asd93d939dm3d";
+        const decoded = jwt.verify(token, jwtSecret);
+        console.log(decoded);
+        req.user = {id: decoded.id, email: decoded.email};
+        next();
+    }
+    catch(error){
+        console.log(error);
+        return res.status(401).json({
+            message: "Invalid or expired token."
+        });
+    }
+}
+
+module.exports = { authMiddleware };
