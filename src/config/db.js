e53d7821e8ff743520cd85e15840158a2f6eb656
Renan Cavalcanti <renan_cavalcanti@hotmail.com>
2026-04-02 20:12:05 -0400

    chore: cors and dotenv

--- src/config/db.js
+++ src/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 
 const connectDB = async () => {
-    const mongoUri = "mongodb+srv://renancavalcanti_db_user:aukO8kTGQc7cGkUy@cluster0.tuzzeep.mongodb.net/?appName=Cluster0/gg-hiver";
+    const mongoUri = process.env.MONGO_URI;
 
     await mongoose.connect(mongoUri);
     console.log("MongoDB connected successfully!");
--- src/server.js
+++ src/server.js
@@ -1,11 +1,18 @@
+const path = require("path");
+const dotenv = require("dotenv");
+
+dotenv.config({path: path.resolve(__dirname, "../.env")});
+
 const { connectDB } = require("./config/db");
 const app = require("./app");
+
+const PORT = process.env.PORT || 5000;
 
 const startServer = async () => {
     try{
         await connectDB();
 
-        app.listen(5000, () => {
+        app.listen(PORT, () => {
             console.log("Server running....");
         });
     }
